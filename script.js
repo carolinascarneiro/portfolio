cab35d3f5fd2786e996944081c5ee09898022df8
Carolina Carneiro <cc@itacs.de>
2022-08-11 18:44:19 +0200

    test code

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-function myFunction() {
-    document.getElementById("home").innerText = "YOU CHANGED ME!";
-}
+let btn = document.getElementById('projects-btn')
 
-let btn = document.getElementById("projects-btn")
-// document.addEventListener('DOMContentLoaded', function () {
-//     projects-btn.addEventListener('click', myFunction, false);
-// });
+btn.addEventListener('click', () => {
+    alert("Hi")
+})
 
-btn.addEventListener('click', myFunction)
-console.log("hi")
 
