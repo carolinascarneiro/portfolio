9a9be47c331294e10d2c6f8e07eccd31f46cbc38
Carolina Carneiro <cc@itacs.de>
2022-08-11 17:42:46 +0200

    pushing differnet js functions

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function myFunction() {
+    document.getElementById("home").innerText = "YOU CHANGED ME!";
+}
+
+let btn = document.getElementById("projects-btn")
+document.addEventListener('DOMContentLoaded', function () {
+    projects-btn.addEventListener('click', myFunction, false);
+});
+
+btn.addEventListener('click', myFunction)
 
