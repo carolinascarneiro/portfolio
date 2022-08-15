1f33c91f1dd8b667ddc9f0298ea64cf016c9b7db
carolinascarneiro <49428998+carolinascarneiro@users.noreply.github.com>
2022-08-15 11:11:41 +0200

    add updated version website with structured contact page and new footer border

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
+
+
 let btn = document.getElementById('projects-btn')
+let home = document.getElementById('home');
+let homebtn = document.getElementById('home-btn')
+let projs = document.getElementById('projs');
+let abprojs = document.getElementById('altbike-projs')
+let about = document.getElementById('about');
+let contact = document.getElementById('contact');
+let aboutbtn = document.getElementById('about-btn');
+let contactbtn = document.getElementById('contact-btn');
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    home.style.display = 'flex';
+    projs.style.display = 'none';
+    about.style.display = 'none';
+    contact.style.display = 'none';
+});
+
+homebtn.addEventListener('click', () => {
+    home.style.display = 'flex';
+    projs.style.display = 'none';
+    abprojs.style.display= 'none';
+    about.style.display = 'none';
+    contact.style.display = 'none';
+});
 
 btn.addEventListener('click', () => {
-    alert("Hi")
-})
+    home.style.display = 'none';
+    projs.style.display = 'flex';
+    abprojs.style.display= 'flex';
+    about.style.display = 'none';
+    contact.style.display = 'none';
+});
+
+aboutbtn.addEventListener('click', () => {
+    home.style.display = 'none';
+    projs.style.display = 'none';
+    abprojs.style.display= 'none';
+    about.style.display = 'flex';
+    contact.style.display = 'none';
+});
+
+contactbtn.addEventListener('click', () => {
+    home.style.display = 'none';
+    projs.style.display = 'none';
+    abprojs.style.display= 'none';
+    about.style.display = 'none';
+    contact.style.display = 'flex';
+});
+
 
 
